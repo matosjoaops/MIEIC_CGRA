d106baca3b9b308a4d1ade01e3f8bcabbcd359e7
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-02-20 18:21:23 +0000

    ex1, items 1 and 2

--- ex1/MyScene.js
+++ ex1/MyScene.js
@@ -21,10 +21,14 @@ class MyScene extends CGFscene {
 
         //Initialize scene objects
         this.axis = new CGFaxis(this);
-        this.diamond = new MyDiamond(this);
+        this.diamond  = new MyDiamond (this);
+        this.triangle = new MyTriangle(this);
 
         //Objects connected to MyInterface
-        this.displayAxis = true;
+        this.displayAxis     = true;
+        this.displayDiamond  = true;
+        this.displayTriangle = true;
+        
         this.scaleFactor = 1;
     }
     initLights() {
@@ -68,7 +72,8 @@ class MyScene extends CGFscene {
 
         // ---- BEGIN Primitive drawing section
 
-        this.diamond.display();
+        if(this.displayDiamond ) this.diamond .display();
+        if(this.displayTriangle) this.triangle.display();
 
         // ---- END Primitive drawing section
     }
