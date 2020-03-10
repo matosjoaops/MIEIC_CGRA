91dcf051271a3dcd6a117d112d38845ee612d637
MechJM <matosjoaops@protonmail.com>
2020-03-10 23:40:46 +0000

    Added most chairs

--- extra/Classroom.js
+++ extra/Classroom.js
@@ -28,6 +28,24 @@ class Island1 extends CGFobject {
             this.scene.translate(-0.51, 0, -0.26);
             this.table.display();
         } this.scene.popMatrix();
+        this.scene.pushMatrix();{
+            this.scene.translate(0.5,-0.1,-0.6);
+            this.chair.display();
+        }this.scene.popMatrix();
+        this.scene.pushMatrix();{
+            this.scene.translate(-0.5,-0.1,-0.6);
+            this.chair.display();
+        }this.scene.popMatrix();
+        this.scene.pushMatrix();{
+            this.scene.translate(-0.5,-0.1,0.6);
+            this.scene.rotate(Math.PI,0,1,0);
+            this.chair.display();
+        }this.scene.popMatrix();
+        this.scene.pushMatrix();{
+            this.scene.translate(0.5,-0.1,0.6);
+            this.scene.rotate(Math.PI,0,1,0);
+            this.chair.display();
+        }this.scene.popMatrix();
     }
 }
 
