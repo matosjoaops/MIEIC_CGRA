f20b431b88aa40b05e8d52d14904c659faed2687
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-12 18:55:46 +0000

    changed script

--- zip-sub.sh
+++ zip-sub.sh
@@ -1,3 +1,8 @@
+# zip-sub
+# usage: ./zip-sub.sh dir num
+# Zips content of folder to submit.
+# dir   directory to zip
+# num   number of submission
 cd $1
 rm -f ../subs/zip/$1-t6g06-$2.zip
 zip -r ../subs/zip/$1-t6g06-$2.zip . -x '*.pdf'
