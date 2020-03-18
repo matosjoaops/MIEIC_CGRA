2d02ff0d23692dfc96cddb139a6bc89f4b8a9e3f
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-18 23:17:16 +0000

    added ex4

--- ex4/MyQuad.js
+++ ex4/MyQuad.js
@@ -10,26 +10,38 @@ class MyQuad extends CGFobject {
 		if (coords != undefined)
 			this.updateTexCoords(coords);
 	}
+	
 	initBuffers() {
 		this.vertices = [
-			0.5, 0.5, 0,	//0
-			-0.5, 0.5, 0,	//1
-            -0.5, -0.5, 0, 	//2
-            0.5, -0.5, 0 //3
-		];
-
-		this.normals = [
-			0, 0, 1,	//0
-			0, 0, 1,	//1
-            0, 0, 1, 	//2
-            0, 0, 1,	//3
+			-0.5, -0.5, 0,	//0
+			0.5, -0.5, 0,	//1
+			-0.5, 0.5, 0,	//2
+			0.5, 0.5, 0		//3
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-            0, 1, 2,
-            2, 3, 0
+			0, 1, 2,
+			1, 3, 2
 		];
+
+		//Facing Z positive
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		];
+		
+		/*
+		Texture coords (s,t)
+		+----------> s
+        |
+        |
+		|
+		v
+        t
+        */
 
 		this.texCoords = [
 			0, 1,
@@ -37,11 +49,7 @@ class MyQuad extends CGFobject {
 			0, 0,
 			1, 0
 		]
-
-		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
-
 		this.initGLBuffers();
 	}
 
