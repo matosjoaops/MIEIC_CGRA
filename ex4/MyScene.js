b613592fdfb5b0319b9cc924177412a28feb90ed
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-19 18:00:43 +0000

    added checkbox

--- ex4/MyScene.js
+++ ex4/MyScene.js
@@ -43,6 +43,7 @@ class MyScene extends CGFscene {
 
         //-------Objects connected to MyInterface
         this.displayAxis = true;
+        this.displayQuad = false;
         this.scaleFactor = 5;
         this.selectedTexture = -1;        
         this.wrapS = 0;
@@ -113,16 +114,17 @@ class MyScene extends CGFscene {
 
         // ---- BEGIN Primitive drawing section
 
-        this.quadMaterial.apply();
+        if(this.displayQuad) {
+            this.quadMaterial.apply();
 
-        // Default texture filtering in WebCGF is LINEAR. 
-        // Uncomment next line for NEAREST when magnifying, or 
-        // add a checkbox in the GUI to alternate in real time
-        
-        // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+            // Default texture filtering in WebCGF is LINEAR. 
+            // Uncomment next line for NEAREST when magnifying, or 
+            // add a checkbox in the GUI to alternate in real time
+            
+            // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
 
-        this.quad.display();
-
+            this.quad.display();
+        }
         // ---- END Primitive drawing section
     }
 }
